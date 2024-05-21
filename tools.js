6d1f9da88f941ddd77c827ba7b03fa2715d16b0d
Ojas Ankit <ojasankit@gmail.com>
2024-05-21 23:11:41 +0530

    Added the sticky note functionality
    1. Added sticky note
    2. Added sticky close
    3. Added sticky minimze
    4. Added moving the sticky through nav bar

--- tools.js
+++ tools.js
@@ -32,16 +32,22 @@ for (let i = 0; i < toolsArr.length; i++) {
     if (toolSelected == "black-pencil") {
       currentTool = "black-pencil";
       tool.strokeStyle = "black";
+      tool.lineWidth = "1";
+      drawWithPencil();
     } else if (toolSelected == "eraser") {
       currentTool = "eraser";
       tool.strokeStyle = "white";
       tool.lineWidth = "10";
+      drawWithPencil();
     } else if (toolSelected == "addStickyNote") {
+      currentTool = "sticky-tool";
+      createSticky();
     } else if (toolSelected == "imageUpload") {
     } else if (toolSelected == "download") {
     } else if (toolSelected == "undo") {
     } else if (toolSelected == "redo") {
     } else if (toolSelected == "trash") {
+      tool.clearRect(0, 0, canvas.width, canvas.height);
     }
   });
 }
@@ -50,36 +56,109 @@ for (let i = 0; i < toolsArr.length; i++) {
 // to implement pencil:
 // mouse pressed on canvas => start point (coordinates)
 // mouse lifted from canvas => end point (coordinates)
+function drawWithPencil() {
+  let drawingFlag = false;
 
-let drawingFlag = false;
+  canvas.addEventListener("mousedown", function (e) {
+    let startX = e.clientX;
+    let startY = e.clientY - toolBar.getBoundingClientRect().height; //subtracting the height of tool bar from Y coordinate
 
-canvas.addEventListener("mousedown", function (e) {
-  let startX = e.clientX;
-  let startY = e.clientY - toolBar.getBoundingClientRect().height; //subtracting the height of tool bar from Y coordinate
+    drawingFlag = true;
+    tool.beginPath();
+    tool.moveTo(startX, startY);
+  });
 
-  drawingFlag = true;
-  tool.beginPath();
-  tool.moveTo(startX, startY);
-});
+  canvas.addEventListener("mousemove", function (e) {
+    if (drawingFlag == false) return;
+    let endX = e.clientX;
+    let endY = e.clientY - toolBar.getBoundingClientRect().height;
 
-canvas.addEventListener("mousemove", function (e) {
-  if (drawingFlag == false) return;
-  let endX = e.clientX;
-  let endY = e.clientY - toolBar.getBoundingClientRect().height;
+    tool.lineTo(endX, endY);
+    tool.stroke();
+  });
 
-  tool.lineTo(endX, endY);
-  tool.stroke();
-});
+  canvas.addEventListener("mouseup", function (e) {
+    if (drawingFlag == false) return;
+    let endX = e.clientX;
+    let endY = e.clientY - toolBar.getBoundingClientRect().height;
 
-canvas.addEventListener("mouseup", function (e) {
-  if (drawingFlag == false) return;
-  let endX = e.clientX;
-  let endY = e.clientY - toolBar.getBoundingClientRect().height;
+    tool.lineTo(endX, endY);
+    tool.stroke();
+    drawingFlag = false;
+  });
+}
 
-  tool.lineTo(endX, endY);
-  tool.stroke();
-  drawingFlag = false;
-});
+function createSticky() {
+  // creating the structure for sticky notes
+  let sticky = document.createElement("div");
+  let stickyNavigation = document.createElement("div");
+  let minimize = document.createElement("div");
+  let close = document.createElement("div");
+  let textArea = document.createElement("textarea");
+
+  let isMinimized = false;
+
+  // adding class for styling
+  sticky.setAttribute("class", "sticky");
+  stickyNavigation.setAttribute("class", "sticky-navigation");
+  textArea.setAttribute("class", "text-area");
+
+  // adding x and min
+  minimize.innerText = "min";
+  close.innerText = "x";
+
+  // creating the tree structure
+  sticky.appendChild(stickyNavigation);
+  sticky.appendChild(textArea);
+  stickyNavigation.appendChild(minimize);
+  stickyNavigation.appendChild(close);
+
+  // adding sticky to page
+  document.body.appendChild(sticky);
+
+  // adding close and minimize div functionality
+  close.addEventListener("click", function () {
+    sticky.remove();
+  });
+
+  minimize.addEventListener("click", function () {
+    textArea.style.display = isMinimized == true ? "block" : "none"; // if minimized -> maximize, if maximized -> minimize
+    isMinimized = !isMinimized;
+  });
+
+  // adding the functionality for moving the sticky note around
+  let isMouseDown = false;
+
+  stickyNavigation.addEventListener("mousedown", function (e) {
+    // initial points
+    initialX = e.clientX;
+    initialY = e.clientY;
+    isMouseDown = true;
+  });
+
+  stickyNavigation.addEventListener("mousemove", function (e) {
+    if (isMouseDown == true) {
+      // final points
+      let finalX = e.clientX;
+      let finalY = e.clientY;
+      console.log(finalX, finalY);
+      // distance moved
+      let dx = finalX - initialX;
+      let dy = finalY - initialY;
+
+      let { top, left } = sticky.getBoundingClientRect();
+      sticky.style.top = top + dy + "px";
+      sticky.style.left = left + dx + "px";
+
+      initialX = finalX;
+      initialY = finalY;
+    }
+  });
+
+  stickyNavigation.addEventListener("mouseup", function (e) {
+    isMouseDown = false;
+  });
+}
 
 // ***** Personal Notes and Comments *******
 
